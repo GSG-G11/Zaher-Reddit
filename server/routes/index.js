ee96f999b0a09d205e2ad6bd4bd11aeaedf73d90
zaher-aa <zaherabuamro@gmail.com>
2022-03-12 20:32:10 +0200

    Handled protected routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const pages = require('./pages');
 const api = require('./api');
+const { clientError } = require('../controllers');
 
+const privateFiles = [/\/html\/*/, /\/css\/*/, /\/js\/*/, /\/images\/*/];
+
+router.use(privateFiles, clientError);
 router.use(pages);
 router.use(api);
+
 module.exports = router;
