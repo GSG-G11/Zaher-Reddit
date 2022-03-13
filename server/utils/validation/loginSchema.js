22c024f59897525b06092e7a5346c4895c9e37f0
zaher-aa <zaherabuamro@gmail.com>
2022-03-13 19:09:45 +0200

    Added login schema Relates #17

---
+++ server/utils/validation/loginSchema.js
@@ -0,0 +1,7 @@
+const Joi = require('joi');
+
+module.exports = Joi.object({
+  username: Joi.string().$.alphanum().rule({ message: 'Username must contain only letters or numbers' }).required(),
+  password: Joi.string().$.min(6).max(33)
+    .rule({ message: 'Password length must be between 6 and 33 characters' }).required(),
+});
