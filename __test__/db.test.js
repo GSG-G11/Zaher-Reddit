17d9942a1c5f045264bb57ae4b122e828a9380ad
zaher-aa <zaherabuamro@gmail.com>
2022-03-13 20:05:46 +0200

    Edited test suites for signup db Relates #17

--- __test__/db.test.js
+++ __test__/db.test.js
@@ -14,30 +14,30 @@ afterAll(() => dbConnection.end());
 describe('Test suits for signup queries', () => {
   it('should create a new user', async () => {
     const data = await createNewUser({
-      username: 'Zaher',
-      email: 'zaher@gmail.com',
+      username: 'Zain',
+      email: 'zain@gmail.com',
       password: '123456',
     });
     expect(data.rows[0].id).toBe(4);
   });
 
   it('should return 0 if the username is available', async () => {
-    const data = await checkUsernameExistsQuery('Zaher');
+    const data = await checkUsernameExistsQuery('Zain');
     expect(data.rows[0].bit).toBe('0');
   });
 
   it('should return 1 if the username is NOT available', async () => {
-    const data = await checkUsernameExistsQuery('john');
+    const data = await checkUsernameExistsQuery('Zaher');
     expect(data.rows[0].bit).toBe('1');
   });
 
   it('should return 0 if the email is available', async () => {
-    const data = await checkEmailExistsQuery('zaher@gmail.com');
+    const data = await checkEmailExistsQuery('zain@gmail.com');
     expect(data.rows[0].bit).toBe('0');
   });
 
   it('should return 1 if the email is NOT available', async () => {
-    const data = await checkEmailExistsQuery('john@gmail.com');
+    const data = await checkEmailExistsQuery('zaher@gmail.com');
     expect(data.rows[0].bit).toBe('1');
   });
 });
