aabe5bf6fc0ec510dd55fb47b43aa9b2b1b8327a
zaher-aa <zaherabuamro@gmail.com>
2022-03-13 19:09:22 +0200

    Implemented login logic Relates #17

---
+++ server/controllers/apis/auth/loginHandler.js
@@ -0,0 +1,25 @@
+const { compare } = require('bcryptjs');
+const { loginSchema, signToken } = require('../../../utils');
+const { checkUsernameQuery } = require('../../../database/queries');
+const { CustomErr } = require('../../error');
+
+module.exports = async ({ body }, res, next) => {
+  const { username, password } = body;
+
+  try {
+    await loginSchema.validateAsync(body);
+    const checkUserQueryPayload = await checkUsernameQuery(username);
+    const userExists = checkUserQueryPayload.rowCount;
+    if (!userExists) throw CustomErr(`User named ${username} does not exist`, 400);
+    const { password: userHashedPassword, id: userId } = checkUserQueryPayload.rows[0];
+    const verifiedUser = await compare(password, userHashedPassword);
+    if (!verifiedUser) throw CustomErr('Invalid Password', 400);
+    const token = await signToken(userId);
+    res
+      .status(201)
+      .cookie('access_token', token)
+      .json({ message: 'Token added successfully', status: 201 });
+  } catch (err) {
+    err.details ? next(CustomErr(err.message, 400)) : next(err);
+  }
+};
