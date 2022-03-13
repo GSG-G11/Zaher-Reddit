28a565a0eb51438fc732db1646e750aa05ecc62a
zaher-aa <zaherabuamro@gmail.com>
2022-03-13 11:18:31 +0200

    Added customerror to index file Relates #12

--- server/controllers/error/index.js
+++ server/controllers/error/index.js
@@ -1,7 +1,9 @@
 const clientError = require('./clientError');
 const serverError = require('./serverError');
+const CustomErr = require('./CustomErr');
 
 module.exports = {
   clientError,
   serverError,
+  CustomErr,
 };
