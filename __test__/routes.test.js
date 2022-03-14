07690c4535d3e00e286d36dbf84bab7489265ee1
zaher-aa <zaherabuamro@gmail.com>
2022-03-14 19:00:46 +0200

    test getAllPosts route Relates #34

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -263,3 +263,17 @@ describe('GET /user', () => {
       });
   });
 });
+
+describe('GET /posts', () => {
+  it('should return 200 OK and Content-Type /json/', (done) => {
+    request(app)
+      .get('/api/v1/posts')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.length).toBe(9);
+        return done();
+      });
+  });
+});
