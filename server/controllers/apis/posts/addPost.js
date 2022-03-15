dbaa951f4bd7d615a089486a0b5d0640ac0fb823
zaher-aa <zaherabuamro@gmail.com>
2022-03-15 17:08:43 +0200

    Added post validation and handled validation errors Relates #27

--- server/controllers/apis/posts/addPost.js
+++ server/controllers/apis/posts/addPost.js
@@ -1,15 +1,18 @@
 const { addPostQuery } = require('../../../database/queries');
+const { addPostSchema } = require('../../../utils');
+const { CustomErr } = require('../../error');
 
 module.exports = async (req, res, next) => {
   const votes = 0;
   const { body: { title, content }, id: userId } = req;
 
   try {
+    await addPostSchema.validateAsync(req.body);
     const { rows } = await addPostQuery({
       userId, title, content, votes,
     });
     res.status(201).json({ status: 201, message: 'Post Added', post: rows[0] });
   } catch (err) {
-    next(err);
+    err.details ? next(CustomErr(err.details[0].message, 400)) : next(err);
   }
 };
