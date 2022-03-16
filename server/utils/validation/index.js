60d6bb53c884386f0b38a6d018cecd4ec81a62eb
zaher-aa <zaherabuamro@gmail.com>
2022-03-16 15:40:18 +0200

    Added comment schema Relates #28

--- server/utils/validation/index.js
+++ server/utils/validation/index.js
@@ -2,10 +2,12 @@ const signupSchema = require('./signupSchema');
 const checkTaken = require('./checkTaken');
 const loginSchema = require('./loginSchema');
 const addPostSchema = require('./addPostSchema');
+const addCommentSchema = require('./addCommentSchema');
 
 module.exports = {
   checkTaken,
   signupSchema,
   loginSchema,
   addPostSchema,
+  addCommentSchema,
 };
